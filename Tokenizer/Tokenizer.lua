17d17f21113789fd2fcc82665c407d1942ae8470
chris <armoredmoose@gmail.com>
2025-05-02 20:36:04 -0500

    Added tokenizer

---
+++ Tokenizer/Tokenizer.lua
@@ -0,0 +1,24 @@
+--[[
+    This file is part of Dysnomia interpreter.
+
+    Dysnomia Interpreter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License only.
+
+    Dysnomia Interpreter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Dysnomia Interpreter. If not, see <https://www.gnu.org/licenses/>.
+
+]]
+
+
+local Tokenizer <const> = {}
+Tokenizer.__index = Tokenizer
+
+_ENV = Tokenizer
+
+
+
+function Tokenizer.tokenize()
+
+end
+
+return Tokenizer
