2687435ddc9c5b795ccdb241f054f4bec72fbecd
chris <armoredmoose@gmail.com>
2025-05-02 20:33:06 -0500

    init

---
+++ main.lua
@@ -0,0 +1,21 @@
+--[[
+
+	Dysnomia interpreter. interpreter for the Dysnomia programing language.
+    Copyright (C) <2025>  github/return5  - chris nichols
+    contact me via github or return5_programming@pm.me
+     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+]]
+
+
+
+local function main()
+
+end
+
+
+main()
