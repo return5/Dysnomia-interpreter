04b63fe79a6c3bc04d961a81a57db611229f74ed
chris <armoredmoose@gmail.com>
2025-05-03 21:05:34 -0500

    added license info

--- tokenizer/comments/TokenizeComment.lua
+++ tokenizer/comments/TokenizeComment.lua
@@ -1,3 +1,12 @@
+--[[
+    This file is part of Dysnomia interpreter.
+
+    Dysnomia Interpreter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License only.
+
+    Dysnomia Interpreter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Dysnomia Interpreter. If not, see <https://www.gnu.org/licenses/>.
+]]
 
 local SingleLimeCommentTokenizer <const> = require('tokenizer.comments.SingleLineCommentTokenizer')
 local RegularMultiLineCommentTokenizer <const> = require('tokenizer.comments.RegularMultiLineCommentTokenizer')
